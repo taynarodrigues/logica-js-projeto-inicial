9dc8ef173b2cad94a77cd235b291ce46dfc35900
taynarodrigues <taynajesus@gmail.com>
2025-03-02 15:20:24 -0400

    instrução break

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ while(chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 10');
     //se chute for igual ao número secreto
     if(chute == numeroSecreto){
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else{
         if(chute > numeroSecreto){
             alert(`O número secreto é menor ${chute}`);
@@ -20,6 +20,11 @@ while(chute != numeroSecreto){
         }
         tentativas = tentativas + 1;
     }
+}
+if(tentativas > 1){
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
 }
 
 
