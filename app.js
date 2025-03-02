92a53834102ce39433fd58be83aa5345198bda37
taynarodrigues <taynajesus@gmail.com>
2025-03-02 01:34:39 -0400

    novas condições

--- app.js
+++ app.js
@@ -2,14 +2,18 @@
 alert('Boas -vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-prompt("Escolha um número entre 1 e 10");
+
 let chute = prompt('Esolha um número entre 1 e 10');
 
 //se chute for igual ao número secreto
 if(chute == numeroSecreto){
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else{
-    alert('Você errou : (');
+    if(chute > numeroSecreto){
+        alert(`O número secreto é menor ${chute}`);
+    }else{
+        alert(`O número secreto é maior ${chute}`)
+    }
 }
 
 
