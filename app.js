481226ca6f2230a1a2895da7740b0d09a58f855e
taynarodrigues <taynajesus@gmail.com>
2025-03-02 17:51:40 -0400

    refatorando o código com operador ternário

--- app.js
+++ app.js
@@ -18,14 +18,12 @@ while(chute != numeroSecreto){
         }else{
             alert(`O número secreto é maior ${chute}`);
         }
-        tentativas = tentativas + 1;
+        tentativas++;
     }
 }
-if(tentativas > 1){
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-}else{
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
-}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
 
 
