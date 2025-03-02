e2fb872b7419a3021d12e1c5e3a110b0830fc5a2
taynarodrigues <taynajesus@gmail.com>
2025-03-02 02:27:15 -0400

    condição do enquanto/while

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ alert('Boas -vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 
-let chute = prompt('Esolha um número entre 1 e 10');
+let chute 
 
-//se chute for igual ao número secreto
-if(chute == numeroSecreto){
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else{
-    if(chute > numeroSecreto){
-        alert(`O número secreto é menor ${chute}`);
-    }else{
-        alert(`O número secreto é maior ${chute}`)
+//enquanto chute não for igual ao núemro secreto
+while(chute != numeroSecreto){
+    chute = prompt('Esolha um número entre 1 e 10');
+    //se chute for igual ao número secreto
+    if(chute == numeroSecreto){
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+    } else{
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor ${chute}`);
+        }else{
+            alert(`O número secreto é maior ${chute}`)
+        }
     }
 }
+
 
 
 
