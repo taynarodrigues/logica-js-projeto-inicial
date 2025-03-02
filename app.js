cf2d7aea7a418486b9ede189411a6014703523f9
taynarodrigues <taynajesus@gmail.com>
2025-03-02 19:08:04 -0400

    aplicando o gerador de números aleatórios: função Math.random()

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 alert('Boas -vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 
 let chute;
@@ -8,7 +8,7 @@ let tentativas = 1;
 
 //enquanto chute não for igual ao núemro secreto
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     //se chute for igual ao número secreto
     if(chute == numeroSecreto){
         break;
